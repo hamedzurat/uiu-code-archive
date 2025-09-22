deed4b71326b1b0ad843393f568feb306621641c
hamed zurat bin hashem <38398613+hamedzurat@users.noreply.github.com>
2025-09-23 00:17:06 +0600

    backup from (Arch Linux) on "2025-09-23--00:17"

--- DSA2_LAB/assignment/0112330113_1.cpp
+++ DSA2_LAB/assignment/0112330113_1.cpp
@@ -2,57 +2,40 @@
 using namespace std;
 
 struct sub {
-    int length, low, high;
+    int size, low, high;
 };
 
-sub crossSubArray(vector<int>& a, int low, int mid, int high) {
-    int L = 0, R = 0;
-    sub cross;
+sub crossSubArray(vector<int>& p, int low, int mid, int high) {
+    int r = 0, l = 0;
+    for(int i = mid + 1; i <= high; i++) {
+        if(p[i] < 0) break;
+        ++l;
+    }
 
     for(int i = mid; i >= low; i--) {
-        if(a[i] > 0)
-            L++;
-        else
-            break;
+        if(p[i] < 0) break;
+        ++r;
     }
 
-    for(int i = mid + 1; i <= high; i++) {
-        if(a[i] > 0)
-            R++;
-        else
-            break;
-    }
-
-    cross.length = L + R;
-    cross.low    = mid + 1 - L;
-    cross.high   = mid + R;
-
-    return cross;
+    return { r + l, mid + 1 - l, mid + r };
 }
 
-sub longsubarray(vector<int>& a, int low, int high) {
+sub longsubarray(vector<int>& p, int low, int high) {
     if(low == high) {
-        sub base;
-
-        if(a[low] > 0) {
-            base.length = 1;
-            base.low = base.high = high;
-        } else {
-            base.length = 0;
-            base.low = base.high = -1;
-        }
-
-        return base;
+        if(p[low] > 0)
+            return { 1, low, high };
+        else
+            return { 0, -1, -1 };
     }
 
     int mid = (low + high) / 2;
-    sub l   = longsubarray(a, low, mid);
-    sub r   = longsubarray(a, mid + 1, high);
-    sub c   = crossSubArray(a, low, mid, high);
+    sub l   = longsubarray(p, low, mid);
+    sub r   = longsubarray(p, mid + 1, high);
+    sub c   = crossSubArray(p, low, mid, high);
 
-    if(l.length >= r.length && l.length >= c.length)
+    if(l.size > c.size && l.size > r.size)
         return l;
-    else if(r.length >= l.length && r.length >= c.length)
+    else if(r.size > c.size && r.size > l.size)
         return r;
     else
         return c;
@@ -63,6 +46,6 @@ int main() {
 
     sub a = longsubarray(p, 0, p.size() - 1);
 
-    cout << a.length << endl;
-    for(int i = a.low; i <= a.high; i++) cout << i + 1 << " ";
+    cout << a.size << endl;
+    for(int i = a.low + 1; i <= a.high + 1; ++i) cout << p[i] << ' ';
 }
