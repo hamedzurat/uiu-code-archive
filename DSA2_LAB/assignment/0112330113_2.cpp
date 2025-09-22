dedad64bd7f7ae9ae5bf0bed8633359823356601
hamed zurat bin hashem <38398613+hamedzurat@users.noreply.github.com>
2025-09-23 00:57:44 +0600

    backup from (Arch Linux) on "2025-09-23--00:57"

--- DSA2_LAB/assignment/0112330113_2.cpp
+++ DSA2_LAB/assignment/0112330113_2.cpp
@@ -3,66 +3,63 @@ using namespace std;
 
 class Item {
     public:
+    double size, total, per;
     string name;
-    double volume, glucose, unitGlucose;
+
+    Item(string n, double s, double t)
+    : name(n), size(s), total(t), per(t / s) {};
     void display() {
-        cout << name << "\t| " << volume << "L\t| " << glucose << "g" << endl;
-    }
-    Item(string n, double v, double g)
-    : name(n), volume(v), glucose(g) {
-        unitGlucose = g / v;
+        cout << name << "\t| " << size << "L\t| " << total << "g" << endl;
     }
 };
 
-bool Compare(Item a, Item b) {
-    return a.unitGlucose < b.unitGlucose;
-}
-
-
-void FracKnapSack(vector<Item> drinks, double capacity) {
-    sort(drinks.begin(), drinks.end(), Compare);
+void FracKnapSack(vector<Item>& shop, double cap) {
+    sort(shop.begin(), shop.end(), [](Item i1, Item i2) { return i1.per < i2.per; });
 
     cout << endl
-         << "sorting before selection::" << endl
+         << "shop after sort::" << endl
          << endl;
-    for(Item& x : drinks) x.display();
+    for(Item& x : shop) x.display();
     cout << "-----------------------" << endl;
 
     vector<Item> sack;
-    for(Item& x : drinks) {
-        if(capacity <= 0) break;
-        if(x.volume <= capacity) {
+
+    for(Item& x : shop) {
+        if(cap <= 0)
+            break;
+        else if(x.size < cap) {
             sack.push_back(x);
-            capacity -= x.volume;
-            x.volume = 0;
+            cap -= x.size;
+            x.size = 0;
         } else {
-            sack.push_back({ x.name, capacity, capacity * x.unitGlucose });
-            x.volume -= capacity;
-            capacity = 0;
+            sack.push_back({ x.name, cap, cap * x.per });
+            x.size -= cap;
+            cap = 0;
         }
     }
 
-    double totalGlucose = 0;
-    for(Item& i : sack) totalGlucose += i.glucose;
-
     cout << endl
-         << "drinks after selection::" << endl
+         << "shop after rob::" << endl
          << endl;
-    for(Item& x : drinks) x.display();
+    for(Item& x : shop) x.display();
     cout << "-----------------------" << endl;
 
+    double T = 0;
+    for(Item& i : sack) T += i.total;
+
+    cout << "\ntotal: " << T << endl;
     cout << endl
-         << "sack after selection::" << endl
+         << "sack after rob::" << endl
          << endl;
-    cout << "Minimum Glucose = " << totalGlucose << " g" << endl;
     for(Item& x : sack) x.display();
+    cout << "-----------------------" << endl;
 }
 
 int main() {
     vector<Item> shop = {
         { "Galxose-D", 3, 75 },
         { "GlucoMax", 4, 80 },
-        { "Sting", 5, 150 }
+        { "Sting....", 5, 150 }
     };
 
     cout << endl
@@ -74,3 +71,4 @@ int main() {
     FracKnapSack(shop, 6);
     return 0;
 }
+
