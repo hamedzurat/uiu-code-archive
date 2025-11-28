9dc018ca1f1d90e71ed00e48c005122f41aa42e9
hamed zurat bin hashem <38398613+hamedzurat@users.noreply.github.com>
2025-11-28 12:38:33 +0600

    supports prettier now

--- format-everythig.sh
+++ format-everythig.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+prettier --write .
+
 find . \( -name "*.c" -o -name "*.cpp" -o -name "*.cc" -o -name "*.cxx" -o -name "*.h" -o -name "*.hpp" -o -name "*.hh" -o -name "*.hxx" \) -exec clang-format -i {} +
 
 echo "Formatting complete."
